ff87e0b4b8e8e49e10d57b9272d5b415db403b5e
Emma Rawson <40120331+nikemrah@users.noreply.github.com>
2023-03-23 21:11:05 +0200

    Update Netflix Data Analytics.sql

--- Netflix Data Analytics.sql
+++ Netflix Data Analytics.sql
@@ -1,32 +1,32 @@
--- Q: How many movie titles are there in the database? 
+/* Q: How many movie titles are there in the database? */
 
-SELECT COUNT(*) 
-    FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info" 
-    WHERE type='Movie';
+SELECT COUNT(*) AS 'Total Number of Movies'
+  FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info" 
+ WHERE type='Movie' ;
 
--- Q: When was the most recent batch of tv shows and/or movies added to the database?
+/* Q: When was the most recent batch of tv shows and/or movies added to the database? **/
 
-SELECT MAX(DATE(date_added))
-    FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info";
+SELECT MAX(DATE(date_added)) AS 'Recent Additions'
+  FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info" ;
 
--- List all the movies and tv shows in alphabetical order.
+/* List all the movies and tv shows in alphabetical order. */
 
-SELECT title
-    FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info"
-    ORDER BY title asc;
+SELECT title AS 'Title'
+  FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info"
+ ORDER BY title ASC ;
 
---Q: Who was the Director for the movie Bright Star?
+/* Q: Who was the Director for the movie Bright Star? */
 
-SELECT people.director, title.title
-    FROM "CharlotteChaze/BreakIntoTech"."netflix_people" people
-    LEFT JOIN "CharlotteChaze/BreakIntoTech"."netflix_titles_info" title
-    ON people.show_id = title.show_id
-    WHERE title.title = 'Bright Star';
+SELECT people.director AS 'Director', title.title AS 'Title'
+  FROM "CharlotteChaze/BreakIntoTech"."netflix_people" people
+       LEFT JOIN "CharlotteChaze/BreakIntoTech"."netflix_titles_info" title
+       ON people.show_id = title.show_id
+          WHERE title.title = 'Bright Star' ;
 
---Q: What is the oldest movie in the database and what year was it made?
+/* Q: What is the oldest movie in the database and what year was it made? */
 
-SELECT title, release_year
-    FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info"
-    WHERE type = 'Movie'
-    ORDER BY release_year ASC
-    LIMIT 1;
+SELECT title AS 'Title', release_year AS 'Release Year'
+  FROM "CharlotteChaze/BreakIntoTech"."netflix_titles_info"
+ WHERE type = 'Movie'
+ ORDER BY release_year ASC
+ LIMIT 1 ;
